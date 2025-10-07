d230145b858b30cb60cd5bfcb0ad3798f911114a
khadarhandule <khadarxamdule807@gmail.com>
2025-10-07 15:08:48 +0100

    my first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Greetings from './components/Greetings';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <>
+      <Greetings name="Khadar" surname="Abdihai" />
+      <Header />
+      <Header />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Greetings.jsx
@@ -0,0 +1,4 @@
+function Greetings(props) {
+    return <h1>Hello, {props.name} {props.surname}!</h1>;
+}
+export default Greetings;
